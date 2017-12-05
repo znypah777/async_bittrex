7d5a65eab28b1e34e021b1344a9434ec885911c5
Halcyon Ramirez <halcyonjuly7@gmail.com>
2017-12-05 11:30:26 -0600

    changed group initialization

--- async_bittrex/groups/protected_group/__init__.py
+++ async_bittrex/groups/protected_group/__init__.py
@@ -1,10 +1,12 @@
 
 
 from async_bittrex.bases.factory import ProtectedSectionBaseFactory
+from async_bittrex.groups.base_group import basegroup_factory
 
 protected_group_factory = ProtectedSectionBaseFactory()
 
 from .v1 import ProtectedGroup_v1_1
 
 
-protected_group_factory.register("v1.1", ProtectedGroup_v1_1)
+protected_group_factory.register("v1.1", ProtectedGroup_v1_1, basegroup_factory.get_version("v1.1"))
+
--- async_bittrex/groups/public_group/__init__.py
+++ async_bittrex/groups/public_group/__init__.py
@@ -1,7 +1,9 @@
 
 from async_bittrex.bases.factory import PublicSectionBaseFactory
+from async_bittrex.groups.base_group import basegroup_factory
+
 
 public_group_factory = PublicSectionBaseFactory()
 
 from .v1 import PublicGroup_v1_1
-public_group_factory.register("v1.1", PublicGroup_v1_1)
+public_group_factory.register("v1.1", PublicGroup_v1_1, basegroup_factory.get_version("v1.1"))
