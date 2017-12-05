2b809df71a2a72870824f435cbb521aa22e134c3
Halcyon Ramirez <halcyonjuly7@gmail.com>
2017-12-05 11:33:01 -0600

    changed register methods for BaseGroup and method Logic for Public and Protected Groups

--- async_bittrex/bases/factory.py
+++ async_bittrex/bases/factory.py
@@ -1,6 +1,6 @@
 import aiohttp
 
-from typing import Any
+from typing import Any, Union, Dict
 
 
 class _BaseFactory:
@@ -8,34 +8,44 @@ class _BaseFactory:
 
         self._versions = {}
 
-
-    def register(self, version, cls):
+    def register(self, version:str , cls:object, group:object) -> None:
         if self._versions.get(version) is None:
-            self._versions[version] = cls
+            self._versions[version] = {"cls": cls,
+                                       "group": group}
 
-    def get_version(self, version):
+    def get_version(self, version:str) -> Union[object, Dict[str, object]]:
         if version in self._versions:
             return self._versions[version]
         raise ValueError("Unknown Version")
+
 
 class PublicSectionBaseFactory(_BaseFactory):
 
     def __call__(self, session:aiohttp.ClientSession, api_secret:str, api_version: str) -> Any:
         version = self.get_version(api_version)
-        return version(session, api_secret, api_version)
-
+        base = version["group"]["group"](session, api_secret, api_version)
+        group = version["group"]["cls"](base)
+        section = version["cls"](group)
+        return section
 
 
 class ProtectedSectionBaseFactory(PublicSectionBaseFactory):
     def __call__(self, session: aiohttp.ClientSession, api_key:str, api_secret: str, api_version: str) -> Any:
         version = self.get_version(api_version)
-        return version(session, api_key, api_secret, api_version)
+        base = version["group"]["group"](session, api_secret, api_version)
+        group = version["group"]["cls"](api_key, base)
+        section = version["cls"](group)
+        return section
 
 
 
 class BaseGroupFactory(_BaseFactory):
-    def __call__(self, session: aiohttp.ClientSession, group_version: str) -> None:
+    def __call__(self, session: aiohttp.ClientSession, group_version: str) -> object:
         version = self.get_version(group_version)
         return version(session, group_version)
+
+    def register(self, version: str, cls: object) -> None:
+        if self._versions.get(version) is None:
+            self._versions[version] = cls
 
 
