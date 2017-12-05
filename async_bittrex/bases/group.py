dad31582cbf31f1e57c430d9ce11adaf1809147e
Halcyon Ramirez <halcyonjuly7@gmail.com>
2017-12-05 11:33:42 -0600

    changed class signature and moved took out some methods

--- async_bittrex/bases/group.py
+++ async_bittrex/bases/group.py
@@ -1,21 +1,12 @@
 import aiohttp
 
 from typing import Dict, Any
-from urllib.parse import urlencode
 
 
 class BaseGroup:
 
-    BASE_URL="https://bittrex.com/api"
-
-    def __init__(self, session: aiohttp.ClientSession, api_version: str):
+    def __init__(self, session: aiohttp.ClientSession):
         self._session = session
-        self._api_version = api_version
-
-    def _generate_url(self, endpoint:str, params:Dict[str, str]=None) -> str:
-        if params:
-            return f"{BaseGroup.BASE_URL}/{self._api_version}{endpoint}?{urlencode(params)}"
-        return f"{BaseGroup.BASE_URL}/{self._api_version}{endpoint}"
 
     async def _post(self, endpoint: str, data: Dict[str, str] = None, **extra: Dict[str, Any]) -> Dict[str, Any]:
         async with self._session.post(endpoint, data=data, **extra) as resp:
@@ -24,5 +15,3 @@ class BaseGroup:
     async def _get(self, endpoint: str, params: Dict[str, str] = None, **extra: Dict[str, Any]) -> Dict[str, Any]:
         async with self._session.get(endpoint, params=params, **extra) as resp:
             return await resp.json()
-
-
