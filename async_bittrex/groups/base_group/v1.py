11c16c5d14a6b418459fbe93f838eb36ff39de09
Halcyon Ramirez <halcyonjuly7@gmail.com>
2017-12-05 22:33:35 -0600

    added comments and new get multiple helper method

--- async_bittrex/groups/base_group/v1.py
+++ async_bittrex/groups/base_group/v1.py
@@ -1,13 +1,17 @@
 from async_bittrex.bases.group import BaseGroup as Base
-from typing import Dict, Any, Optional
+from typing import Dict, Any, Optional, List, Callable, Awaitable, Union
 from urllib.parse import urlencode
 
 import hmac
 import hashlib
 import aiohttp
+import asyncio
 
 
 class BaseGroup_v1_1(Base):
+    """
+        this class contains methods that are used by both the public and protected groups
+    """
     BASE_URL = "https://bittrex.com/api"
 
     def __init__(self, session: aiohttp.ClientSession, api_secret:str, api_version: str):
@@ -17,11 +21,18 @@ class BaseGroup_v1_1(Base):
 
 
     def _generate_url(self, endpoint:str, params:Dict[str, str]=None) -> str:
+        """
+            generates the url format for a specific endpoint and the endpoint's params
+        """
         if params:
             return f"{BaseGroup_v1_1.BASE_URL}/{self._api_version}{endpoint}?{urlencode(params)}"
         return f"{BaseGroup_v1_1.BASE_URL}/{self._api_version}{endpoint}"
 
     def _gen_api_sig(self, endpoint: str) -> str:
+        """
+            a new feature of v1.1 creates an HMAC hash from a given endpoint
+            the endpoint needs to already be formatted with all its necessary arguments already urlencoded
+        """
         return hmac.new(self._api_secret.encode(),
                         endpoint.encode(),
                         hashlib.sha512).hexdigest()
@@ -30,9 +41,25 @@ class BaseGroup_v1_1(Base):
                          endpoint: str,
                          params: Optional[Dict[str, str]]=None,
                          extra_headers: Optional[Dict[str, str]]=None) -> Dict[str, str]:
+        """
+            Does the actual query which formats the endpoint and creates the HMAC hash for the endpoint
+        """
         target = self._generate_url(endpoint,params=params)
         api_sig = self._gen_api_sig(target)
         headers = {"apisign": api_sig}
         if extra_headers is not None:
             headers.update(extra_headers)
         return await self._get(target, headers=headers)
+
+
+    async def get_multiple(self,
+                           items: List[str],
+                           call_back: Callable[[str], Awaitable[List[Dict[str, str]]]],
+                           extra_headers:Optional[Dict[str, Any]]=None) -> List[Dict[str ,str]]:
+        """
+            Helper method to do bulk call on certain endpoints that only has one required field
+        """
+        tasks = []
+        for item in items:
+            tasks.append(call_back(item, extra_headers=extra_headers))
+        return await asyncio.gather(*tasks)
