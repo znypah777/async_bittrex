3597c751b1dd0bb3d2b3b76ca6a9af91203b5752
halcyonjuuly7 <halcyonjuly7@gmail.com>
2017-12-16 14:32:08 -0600

    removed git merge conflicts

--- async_bittrex/groups/base_group/__init__.py
+++ async_bittrex/groups/base_group/__init__.py
@@ -1,23 +1,8 @@
-
-<<<<<<< HEAD
 from async_bittrex.bases.factory import InternalGroupFactory
 
-
-
 internalgroup_factory = InternalGroupFactory()
-
 
 from .v1 import BaseGroup_v1_1, BaseGroupV2_2
 internalgroup_factory.register("v1.1", BaseGroup_v1_1)
 internalgroup_factory.register("v2.0", BaseGroupV2_2)
-=======
-from async_bittrex.bases.factory import BaseGroupFactory
 
-
-
-basegroup_factory = BaseGroupFactory()
-
-
-from .v1 import BaseGroup_v1_1
-basegroup_factory.register("v1.1", BaseGroup_v1_1)
->>>>>>> 2848be7fbd43c38bbf7a58905e918fd5508c0b4b
