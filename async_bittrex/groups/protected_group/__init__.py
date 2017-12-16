fdebcba36cb63debe74dd1f1fa5eed6b2d88fc12
halcyonjuuly7 <halcyonjuly7@gmail.com>
2017-12-16 14:32:40 -0600

    removed git merge conflicts

--- async_bittrex/groups/protected_group/__init__.py
+++ async_bittrex/groups/protected_group/__init__.py
@@ -1,23 +1,10 @@
-
-
-<<<<<<< HEAD
 from async_bittrex.bases.factory import AccessLevelGroupFactory
 from async_bittrex.groups.base_group import internalgroup_factory
 
 protected_group_factory = AccessLevelGroupFactory()
-=======
-from async_bittrex.bases.factory import ProtectedSectionBaseFactory
-from async_bittrex.groups.base_group import basegroup_factory
-
-protected_group_factory = ProtectedSectionBaseFactory()
->>>>>>> 2848be7fbd43c38bbf7a58905e918fd5508c0b4b
 
 from .v1 import ProtectedGroup_v1_1
 
 
-<<<<<<< HEAD
 protected_group_factory.register("v1.1", ProtectedGroup_v1_1, internalgroup_factory.get_version("v1.1"))
-=======
-protected_group_factory.register("v1.1", ProtectedGroup_v1_1, basegroup_factory.get_version("v1.1"))
->>>>>>> 2848be7fbd43c38bbf7a58905e918fd5508c0b4b
 
--- async_bittrex/groups/public_group/__init__.py
+++ async_bittrex/groups/public_group/__init__.py
@@ -1,5 +1,3 @@
-
-<<<<<<< HEAD
 from async_bittrex.bases.factory import AccessLevelGroupFactory
 from async_bittrex.groups.base_group import internalgroup_factory
 
@@ -10,13 +8,3 @@ from .v1 import PublicGroup_v1_1
 
 public_group_factory.register("v1.1", PublicGroup_v1_1, internalgroup_factory.get_version("v1.1"))
 public_group_factory.register("v2.0", PublicGroup_v1_1, internalgroup_factory.get_version("v2.0"))
-=======
-from async_bittrex.bases.factory import PublicSectionBaseFactory
-from async_bittrex.groups.base_group import basegroup_factory
-
-
-public_group_factory = PublicSectionBaseFactory()
-
-from .v1 import PublicGroup_v1_1
-public_group_factory.register("v1.1", PublicGroup_v1_1, basegroup_factory.get_version("v1.1"))
->>>>>>> 2848be7fbd43c38bbf7a58905e918fd5508c0b4b
